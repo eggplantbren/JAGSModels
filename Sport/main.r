ecdda83d4399163b93d031445c1922e49058424f
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-09-25 11:02:35 +1200

    Three models

--- Sport/main.r
+++ Sport/main.r
@@ -5,13 +5,13 @@ data <- source('data.txt')$value
 
 # now to send the model and data to JAGS:
 # if you need to navigate to the place you saved your .bug file, now is the time to do it.
-m = jags.model(file = "model2.txt", data=data)
+m = jags.model(file = "model3.txt", data=data)
 
 # this will create a BUGS object in R. Now you can do some runs...
 # update(m, 10000) # I found it runs really quickly...
 
 # finally, get the samples from JAGS:
-draw = coda.samples(m, 10000, thin=10, variable.names = c("ability", "advantage", "prob_next"))
+draw = coda.samples(m, 100000, thin=10, variable.names = c("ability", "sig", "prob_next", "advantage"))
 
 # and get a pretty picture and summary stats:
 #plot(draw); summary(draw)
