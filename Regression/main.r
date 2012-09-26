710cad4cdf88eeddddd3820f1fad619356f47733
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-09-26 20:16:11 +1200

    Outlier model

--- Regression/main.r
+++ Regression/main.r
@@ -5,13 +5,13 @@ data <- source('data.txt')$value
 
 # now to send the model and data to JAGS:
 # if you need to navigate to the place you saved your .bug file, now is the time to do it.
-m = jags.model(file="model.txt", data=data)
+m = jags.model(file="outliers.txt", data=data)
 
 # this will create a BUGS object in R. Now you can do some runs...
 update(m, 10000) # I found it runs really quickly...
 
 # finally, get the samples from JAGS:
-draw = coda.samples(m, 100000, thin=30, variable.names = c("beta0", "beta1", "sig", "y_new"))
+draw = coda.samples(m, 10000, thin=1, variable.names = c("beta0", "beta1", "bad_fraction", "log_sig_good", "log_sig_bad"))
 
 # and get a pretty picture and summary stats:
 #plot(draw); summary(draw)
