5ba4880a5941259adc0734ee9ed83574c5b3d528
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-10-02 21:37:17 +1300

    AR(1) sampling distribution

---
+++ TimeSeries/main.r
@@ -0,0 +1,20 @@
+library('rjags')
+
+# create the data to be sent to R (using the same as class example):
+data <- source('nzd.txt')$value
+
+# now to send the model and data to JAGS:
+# if you need to navigate to the place you saved your .bug file, now is the time to do it.
+m = jags.model(file="model.txt", data=data)
+
+# this will create a BUGS object in R. Now you can do some runs...
+# update(m, 10000) # I found it runs really quickly...
+
+# finally, get the samples from JAGS:
+draw = coda.samples(m, 10000, thin=10, variable.names = c("y_future"))
+
+# and get a pretty picture and summary stats:
+#plot(draw); summary(draw)
+
+samples <- as.matrix(draw)
+
